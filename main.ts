0e261499c75cb00110f6c6069c42194834e60d66
Zack <66165382+yoloyoyo@users.noreply.github.com>
2020-11-11 17:44:06 +0800

    left right animations

--- main.ts
+++ main.ts
@@ -10,40 +10,76 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     anim = animation.createAnimation(ActionKind.Walking, 100)
     animation.attachAnimation(mySprite, anim)
     anim.addAnimationFrame(img`
-        . . . . 5 5 5 5 5 5 5 . . . . . 
-        . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        5 5 5 5 f 5 5 5 5 5 5 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-        . . . . 5 5 5 5 5 5 5 . . . . . 
-        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . . . f e 4 4 4 e e f f . . . . 
+        . . . f 2 2 2 e d d 4 . . . . . 
+        . . . f 2 2 2 e d d e . . . . . 
+        . . . f 5 5 4 f e e f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . . f f f . . . . . . . 
         `)
     anim.addAnimationFrame(img`
-        . . . . 5 5 5 5 5 5 5 . . . . . 
-        . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        5 5 5 5 f 5 5 5 5 5 5 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        . . . . . . 5 5 5 5 5 5 5 5 5 . 
-        . . . . . 5 5 5 5 5 5 5 5 5 5 . 
-        . . . . 5 5 5 5 5 5 5 5 5 5 5 . 
-        . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-        . . . . 5 5 5 5 5 5 5 . . . . . 
         . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d e e e e e f . . . 
+        . . . f e 4 e d d 4 f . . . . . 
+        . . . f 2 2 e d d e f . . . . . 
+        . . f f 5 5 f e e f f f . . . . 
+        . . f f f f f f f f f f . . . . 
+        . . . f f f . . . f f . . . . . 
+        `)
+    anim.addAnimationFrame(img`
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . . . f e 4 4 4 e e f f . . . . 
+        . . . f 2 2 2 e d d 4 . . . . . 
+        . . . f 2 2 2 e d d e . . . . . 
+        . . . f 5 5 4 f e e f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        `)
+    anim.addAnimationFrame(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . . . f e 4 4 4 e d d 4 . . . . 
+        . . . f 2 2 2 2 e d d e . . . . 
+        . . f f 5 5 4 4 f e e f . . . . 
+        . . f f f f f f f f f f . . . . 
+        . . . f f f . . . f f . . . . . 
         `)
     animation.setAction(mySprite, ActionKind.Walking)
 })
@@ -78,40 +114,76 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     anim = animation.createAnimation(ActionKind.Walking, 100)
     animation.attachAnimation(mySprite, anim)
     anim.addAnimationFrame(img`
-        . . . . 5 5 5 5 5 5 5 . . . . . 
-        . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        5 5 5 5 5 5 5 5 5 5 f 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 . . . . . . . 
-        5 5 5 5 5 5 5 5 5 5 5 . . . . . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 . . . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-        . . . . 5 5 5 5 5 5 5 . . . . . 
         . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . 4 d d e 4 4 4 e f . . . 
+        . . . . e d d e 2 2 2 2 f . . . 
+        . . . . f e e f 4 4 5 5 f f . . 
+        . . . . f f f f f f f f f f . . 
+        . . . . . f f . . . f f f . . . 
         `)
     anim.addAnimationFrame(img`
-        . . . . 5 5 5 5 5 5 5 . . . . . 
-        . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        5 5 5 5 5 5 5 5 5 5 f 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-        . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-        . . . . 5 5 5 5 5 5 5 . . . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . f f e e 4 4 4 e f . . . 
+        . . . . . 4 d d e 2 2 2 f . . . 
+        . . . . . e d d e 2 2 2 f . . . 
+        . . . . . f e e f 4 5 5 f . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        `)
+    anim.addAnimationFrame(img`
         . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e e e d d d f . . . 
+        . . . . . f 4 d d e 4 e f . . . 
+        . . . . . f e d d e 2 2 f . . . 
+        . . . . f f f e e f 5 5 f f . . 
+        . . . . f f f f f f f f f f . . 
+        . . . . . f f . . . f f f . . . 
+        `)
+    anim.addAnimationFrame(img`
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . f f e e 4 4 4 e f . . . 
+        . . . . . 4 d d e 2 2 2 f . . . 
+        . . . . . e d d e 2 2 2 f . . . 
+        . . . . . f e e f 4 5 5 f . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . . f f f . . . . . . 
         `)
     animation.setAction(mySprite, ActionKind.Walking)
 })
@@ -230,3 +302,6 @@ for (let index = 0; index < 2; index++) {
     createEnemies()
 }
 info.setLife(3)
+game.onUpdate(function () {
+	
+})
