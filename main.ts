e2a3e8a1a7bac3e20b7dccd5c5835da4c4967788
Zack <66165382+yoloyoyo@users.noreply.github.com>
2020-11-11 17:17:06 +0800

    changed character

--- main.ts
+++ main.ts
@@ -157,24 +157,25 @@ let yumyum: Sprite = null
 let badguy: Sprite = null
 let canEatEnemy = false
 let anim: animation.Animation = null
+let Power_Up: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
-    . . . . 5 5 5 5 5 5 5 . . . . . 
-    . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-    . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-    . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-    5 5 5 5 f 5 5 5 5 5 5 5 5 5 5 . 
-    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-    . . . . . . 5 5 5 5 5 5 5 5 5 . 
-    . . . . . 5 5 5 5 5 5 5 5 5 5 . 
-    . . . . 5 5 5 5 5 5 5 5 5 5 5 . 
-    . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-    . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-    . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-    . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-    . . . . 5 5 5 5 5 5 5 . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
 tiles.setTilemap(tiles.createTilemap(hex`1000100002020202020202020202020202020202020202010202020202010101010101020202020102020202020102020202010202010101010102020201020202020202020202020201020201010101020202020202020202010202020201010202020202020101010101010202020101010102020202020201020202020202010202020202020202010202020202020102020202020202010101010101010101010102020202020201020201020202010202020202020202010202010202020102020202020101010102020202020201020202020202020201020202020202020202020202020202010101010101020202020202020202020202020202020202020202`, img`
@@ -201,25 +202,27 @@ info.startCountdown(30)
 for (let index = 0; index < 30; index++) {
     createFood()
 }
-let Power_Up = sprites.create(img`
-    . . . . . . . 6 . . . . . . . . 
-    . . . . . . 8 6 6 . . . 6 8 . . 
-    . . . e e e 8 8 6 6 . 6 7 8 . . 
-    . . e 2 2 2 2 e 8 6 6 7 6 . . . 
-    . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
-    . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
-    e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
-    e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
-    e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
-    e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
-    e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
-    e 2 2 2 2 2 2 2 4 e 2 e e c . . 
-    e e 2 e 2 2 4 2 2 e e e c . . . 
-    e e e e 2 e 2 2 e e e c . . . . 
-    e e e 2 e e c e c c c . . . . . 
-    . c c c c c c c . . . . . . . . 
-    `, SpriteKind.yeet)
-tiles.placeOnRandomTile(Power_Up, sprites.castle.tileGrass2)
+for (let index = 0; index < 4; index++) {
+    Power_Up = sprites.create(img`
+        . . . . . . . . . . . 6 6 6 6 6 
+        . . . . . . . . . 6 6 7 7 7 7 8 
+        . . . . . . 8 8 8 7 7 8 8 6 8 8 
+        . . e e e e c 6 6 8 8 . 8 7 8 . 
+        . e 2 5 4 2 e c 8 . . . 6 7 8 . 
+        e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
+        e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
+        e 2 e e 2 2 2 2 e e e e c 6 8 . 
+        c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
+        . c 2 e e e 2 e 2 4 2 2 2 2 c . 
+        . . c 2 2 2 e e 2 2 2 2 2 2 2 e 
+        . . . e c c e c 2 2 2 2 2 2 2 e 
+        . . . . . . . c 2 e e 2 2 e 2 c 
+        . . . . . . . c e e e e e e 2 c 
+        . . . . . . . . c e 2 2 2 2 c . 
+        . . . . . . . . . c c c c c . . 
+        `, SpriteKind.yeet)
+    tiles.placeOnRandomTile(Power_Up, sprites.castle.tileGrass2)
+}
 for (let index = 0; index < 4; index++) {
     createEnemies()
 }
