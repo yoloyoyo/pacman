2dc027c8d54ea4147d22a250b0ed31a555eedfeb
Zack <66165382+yoloyoyo@users.noreply.github.com>
2020-11-11 18:09:32 +0800

    Added animations all directions

--- main.ts
+++ main.ts
@@ -6,6 +6,86 @@ enum ActionKind {
 namespace SpriteKind {
     export const yeet = SpriteKind.create()
 }
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    anim = animation.createAnimation(ActionKind.Walking, 100)
+    animation.attachAnimation(mySprite, anim)
+    anim.addAnimationFrame(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . f f f f f 2 2 f f f f f . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f 2 f 2 e f . . 
+        . . f f f 2 2 e e 2 2 f f f . . 
+        . f f e f 2 f e e f 2 f e f f . 
+        . f e e f f e e e e f e e e f . 
+        . . f e e e e e e e e e e f . . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `)
+    anim.addAnimationFrame(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . . f f f f 2 2 f f f f . . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f f 2 f e f . . 
+        . . f f f 2 f e e 2 2 f f f . . 
+        . . f e 2 f f e e 2 f e e f . . 
+        . f f e f f e e e f e e e f f . 
+        . f f e e e e e e e e e e f f . 
+        . . . f e e e e e e e e f . . . 
+        . . . e f f f f f f f f 4 e . . 
+        . . . 4 f 2 2 2 2 2 e d d 4 . . 
+        . . . e f f f f f f e e 4 . . . 
+        . . . . f f f . . . . . . . . . 
+        `)
+    anim.addAnimationFrame(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . f f f f f 2 2 f f f f f . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f 2 f 2 e f . . 
+        . . f f f 2 2 e e 2 2 f f f . . 
+        . f f e f 2 f e e f 2 f e f f . 
+        . f e e f f e e e e f e e e f . 
+        . . f e e e e e e e e e e f . . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `)
+    anim.addAnimationFrame(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . . f f f f 2 2 f f f f . . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e f 2 f f f 2 f 2 e f . . 
+        . . f f f 2 2 e e f 2 f f f . . 
+        . . f e e f 2 e e f f 2 e f . . 
+        . f f e e e f e e e f f e f f . 
+        . f f e e e e e e e e e e f f . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f e . . . 
+        . . 4 d d e 2 2 2 2 2 f 4 . . . 
+        . . . 4 e e f f f f f f e . . . 
+        . . . . . . . . . f f f . . . . 
+        `)
+    animation.setAction(mySprite, ActionKind.Walking)
+})
+controller.anyButton.onEvent(ControllerButtonEvent.Released, function () {
+    animation.setAction(mySprite, ActionKind.Idle)
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     anim = animation.createAnimation(ActionKind.Walking, 100)
     animation.attachAnimation(mySprite, anim)
@@ -102,7 +182,7 @@ function createPowerUp () {
         . . . . . . . . c e 2 2 2 2 c . 
         . . . . . . . . . c c c c c . . 
         `, SpriteKind.yeet)
-    tiles.placeOnRandomTile(Power_Up, sprites.castle.tileGrass2)
+    tiles.placeOnRandomTile(Power_Up, sprites.castle.tilePath5)
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.yeet, function (sprite, otherSprite) {
     canEatEnemy = true
@@ -206,32 +286,109 @@ function createEnemies () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         `, SpriteKind.Enemy)
-    tiles.placeOnRandomTile(badguy, sprites.castle.tileGrass2)
+    tiles.placeOnRandomTile(badguy, sprites.castle.tilePath5)
     badguy.follow(mySprite, 50)
 }
 function createFood () {
     yumyum = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . 1 1 1 1 1 . . . . . . 
-        . . . . . 1 1 1 1 1 . . . . . . 
-        . . . . . 1 1 1 1 1 . . . . . . 
-        . . . . . 1 1 1 1 1 . . . . . . 
-        . . . . . 1 1 1 1 1 . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
+        . . . . c c c b b b b b . . . . 
+        . . c c b 4 4 4 4 4 4 b b b . . 
+        . c c 4 4 4 4 4 5 4 4 4 4 b c . 
+        . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+        e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+        e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+        e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+        . e b 4 4 4 4 4 5 4 4 4 4 b e . 
+        8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+        8 7 2 e e e e e e e e e e 2 7 8 
+        e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+        e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+        e b e 8 8 c c 8 8 c c c 8 e b e 
+        e e b e c c e e e e e c e b e e 
+        . e e b b 4 4 4 4 4 4 4 4 e e . 
+        . . . c c c c c e e e e e . . . 
         `, SpriteKind.Food)
-    tiles.placeOnRandomTile(yumyum, sprites.castle.tileGrass2)
+    tiles.placeOnRandomTile(yumyum, sprites.castle.tilePath5)
 }
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    anim = animation.createAnimation(ActionKind.Walking, 100)
+    animation.attachAnimation(mySprite, anim)
+    anim.addAnimationFrame(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `)
+    anim.addAnimationFrame(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . f f e 2 f f f f f f 2 e f f . 
+        . f f f f f e e e e f f f f f . 
+        . . f e f b f 4 4 f b f e f . . 
+        . . f e 4 1 f d d f 1 4 e f . . 
+        . . . f e 4 d d d d 4 e f e . . 
+        . . f e f 2 2 2 2 e d d 4 e . . 
+        . . e 4 f 2 2 2 2 e d d e . . . 
+        . . . . f 4 4 5 5 f e e . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f . . . . . . . . . 
+        `)
+    anim.addAnimationFrame(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `)
+    anim.addAnimationFrame(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f e e 2 2 2 2 2 2 e f f . . 
+        . f f e 2 f f f f f f 2 e f f . 
+        . f f f f f e e e e f f f f f . 
+        . . f e f b f 4 4 f b f e f . . 
+        . . f e 4 1 f d d f 1 4 e f . . 
+        . . e f e 4 d d d d 4 e f . . . 
+        . . e 4 d d e 2 2 2 2 f e f . . 
+        . . . e d d e 2 2 2 2 f 4 e . . 
+        . . . . e e f 5 5 4 4 f . . . . 
+        . . . . . f f f f f f f . . . . 
+        . . . . . . . . . f f f . . . . 
+        `)
+    animation.setAction(mySprite, ActionKind.Walking)
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    if (info.score() == 10) {
+    if (info.score() == 30) {
         game.over(true)
     } else {
         otherSprite.destroy()
@@ -243,7 +400,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
         otherSprite.destroy()
     } else {
         info.changeLifeBy(-1)
-        tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass2)
+        tiles.placeOnRandomTile(mySprite, sprites.castle.tilePath5)
     }
 })
 let yumyum: Sprite = null
@@ -271,25 +428,25 @@ mySprite = sprites.create(img`
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
-tiles.setTilemap(tiles.createTilemap(hex`1000100002020202020202020202020202020202020202010202020202010101010101020202020102020202020102020202010202010101010102020201020202020202020202020201020201010101020202020202020202010202020201010202020202020101010101010202020101010102020202020201020202020202010202020202020202010202020202020102020202020202010101010101010101010102020202020201020201020202010202020202020202010202010202020102020202020101010102020202020201020202020202020201020202020202020202020202020202010101010101020202020202020202020202020202020202020202`, img`
+tiles.setTilemap(tiles.createTilemap(hex`10001000020303030303030303030303030303040901010101010101010101010101010509010a0a0a0a0a010a0a0a0a0a0a010509010a010101010101010101010a01050901010101010101010a0101010a010509010a0101010a01010101010101010509010a010101010101010101010a010509010a010a0a0a010a0a010a0a0a010509010a0101010a01010a0101010a010509010a0a0a010a01010a0a0a010a01050901010101010a0a0a0a0101010a010509010a0101010101010101010101010509010a01010a010101010a01010a010509010a0a0a0a0a0a010a0a0a0a0a01050901010101010101010101010101010507080808080808080808080808080806`, img`
     . . . . . . . . . . . . . . . . 
-    . . . 2 . . . . . 2 2 2 2 2 2 . 
-    . . . 2 . . . . . 2 . . . . 2 . 
-    . 2 2 2 2 2 . . . 2 . . . . . . 
-    . . . . . 2 . . 2 2 2 2 . . . . 
-    . . . . . 2 . . . . 2 2 . . . . 
-    . . 2 2 2 2 2 2 . . . 2 2 2 2 . 
-    . . . . . 2 . . . . . . 2 . . . 
-    . . . . . 2 . . . . . . 2 . . . 
-    . . . . 2 2 2 2 2 2 2 2 2 2 2 . 
-    . . . . . 2 . . 2 . . . 2 . . . 
-    . . . . . 2 . . 2 . . . 2 . . . 
-    . . 2 2 2 2 . . . . . . 2 . . . 
-    . . . . . 2 . . . . . . . . . . 
-    . . . . . 2 2 2 2 2 2 . . . . . 
     . . . . . . . . . . . . . . . . 
-    `, [myTiles.tile0,sprites.castle.tilePath5,sprites.castle.tileGrass2], TileScale.Sixteen))
-tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass2)
+    . . 2 2 2 2 2 . 2 2 2 2 2 2 . . 
+    . . 2 . . . . . . . . . . 2 . . 
+    . . . . . . . . . 2 . . . 2 . . 
+    . . 2 . . . 2 . . . . . . . . . 
+    . . 2 . . . . . . . . . . 2 . . 
+    . . 2 . 2 2 2 . 2 2 . 2 2 2 . . 
+    . . 2 . . . 2 . . 2 . . . 2 . . 
+    . . 2 2 2 . 2 . . 2 2 2 . 2 . . 
+    . . . . . . 2 2 2 2 . . . 2 . . 
+    . . 2 . . . . . . . . . . . . . 
+    . . 2 . . 2 . . . . 2 . . 2 . . 
+    . . 2 2 2 2 2 2 . 2 2 2 2 2 . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, [myTiles.tile0,sprites.castle.tilePath5,sprites.castle.tilePath1,sprites.castle.tilePath2,sprites.castle.tilePath3,sprites.castle.tilePath6,sprites.castle.tilePath9,sprites.castle.tilePath7,sprites.castle.tilePath8,sprites.castle.tilePath4,sprites.builtin.forestTiles0], TileScale.Sixteen))
+tiles.placeOnRandomTile(mySprite, sprites.castle.tilePath5)
 scene.cameraFollowSprite(mySprite)
 info.startCountdown(30)
 for (let index = 0; index < 30; index++) {
@@ -302,6 +459,3 @@ for (let index = 0; index < 2; index++) {
     createEnemies()
 }
 info.setLife(3)
-game.onUpdate(function () {
-	
-})
